016f79a52ad1e07dadbeebcb7675a28c5d96a11f
vaibhav <vaibhav.w@mypcot.com>
2024-05-03 18:52:08 +0530

    start api with no internet screen handled

--- test/helper/helper_test.dart
+++ test/helper/helper_test.dart
@@ -3,6 +3,8 @@ import 'package:dummy_json/core/utils/header/headers.dart';
 import 'package:dummy_json/feature/address/data/profile_model/result.dart';
 import 'package:dummy_json/feature/address/data/repository/profile_repository.dart';
 import 'package:dummy_json/feature/address/data/services/profile_service.dart';
+import 'package:dummy_json/feature/onboarding/data/repository/startup_repository.dart';
+import 'package:dummy_json/feature/onboarding/data/services/startup_services.dart';
 import 'package:dummy_json/feature/suggest_question/data/repository/suggest_question_repository.dart';
 import 'package:dummy_json/feature/suggest_question/data/services/suggest_question_service.dart';
 import 'package:mockito/annotations.dart';
@@ -11,13 +13,16 @@ import 'package:http/http.dart' as http;
 @GenerateMocks(
   [
     ApiHelper,
-    Header,
     ProfileRepository,
     ProfileServices,
-
+    HeaderClass,
     //--
     SuggestQuestionRepository,
     SuggestQuestionService,
+
+    //--
+    StartupServices,
+    OnBoardingRepository,
 
     // Profile model
     Result,
